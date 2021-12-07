b0108cbc19fec394fdb67e461f62e0a1d0dabee9
ugonani10 <ugonani10@gmail.com>
2021-12-07 15:43:35 +0100

    Premiers test sur getter

---
+++ TP4/Models/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP4.Models
+{
+    public class Task
+    {
+        public string Description { get; set; }
+        public int Id { get; set; }
+        public TaskStatus Status { get; set; }
+    }
+    public enum TaskStatus
+    {
+        todo,
+        done
+    }
+}
