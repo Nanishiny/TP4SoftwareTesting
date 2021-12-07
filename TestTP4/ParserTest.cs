56d32fee31dc26304fe28a04a50389c3d2a6e7bf
ugonani10 <ugonani10@gmail.com>
2021-12-07 15:56:03 +0100

    Constructor Parser work

---
+++ TestTP4/ParserTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using TP4.Models;
+
+namespace TP4
+{
+    [TestFixture]
+    class ParserTest
+    {
+        private Parser parser;
+
+        [SetUp]
+        public void Setup()
+        {
+            parser = new Parser();
+        }
+
+        [Test]
+        public void InputPlus()
+        {
+
+        }
+      
+    }
+}
